c7dbecbe9ec826cee8390bee1836b522f29917d4
hiranurtozar <tozarhiranur@gmail.com>
2026-02-03 22:46:46 +0300

    .

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,7 +16,7 @@
             margin: 0 auto;
             background-color: white;
             padding: 20px;
-            border-radius: 8px;
+            border-radius: 8px;gir 
             box-shadow: 0 2px 4px rgba(0,0,0,0.1);
         }
         h1 {
